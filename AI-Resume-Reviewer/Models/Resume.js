63f878883d4d693be7b3c602e968e9171b0a0c72
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-04 20:46:56 +0530

    Update Resume.js

--- AI-Resume-Reviewer/Models/Resume.js
+++ AI-Resume-Reviewer/Models/Resume.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
+
+const resumeSchema = new mongoose.Schema({
+    filename: String,         // name of uploaded file
+    feedback: String,         // AI feedback
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Resume", resumeSchema);
 
